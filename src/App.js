305d5b92adc5f73ede88ba91f94346f6934b7943
steeper <portland.steeper@gmail.com>
2023-07-24 01:20:26 -0400

    working on tailwind, does not seem to be working

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold bg-black">Welcome to My Personal Website</h1>
+      testing
+    </div>
+  );
+};
+
+export default App;
